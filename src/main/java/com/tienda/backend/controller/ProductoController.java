6d55e06df2e7d1d9c1fea43488763c78224325bb
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-02-05 05:56:44 -0500

    Correccion de los CORS

--- src/main/java/com/tienda/backend/controller/ProductoController.java
+++ src/main/java/com/tienda/backend/controller/ProductoController.java
@@ -4,9 +4,13 @@ import com.tienda.backend.model.Producto;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ProductoController {
 
