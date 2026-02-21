691f1232b3b65df605f606d5faff750fe39913ab
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-02-21 05:51:18 -0500

    agregue el id unico a cada producto

--- src/main/java/com/tienda/backend/service/CarritoService.java
+++ src/main/java/com/tienda/backend/service/CarritoService.java
@@ -5,22 +5,26 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;//id UNICO
+
 @Service
 public class CarritoService {
     private List<Producto> carrito;
+    private AtomicLong contador = new AtomicLong(1);//id UNICO
 
     public CarritoService(){
         this.carrito =new ArrayList();
     }
 
     public void agregarProducto(Producto producto) {
-        if(carrito.size()>=10){
-            throw new RuntimeException("Maximo 3 productos");
+        if(carrito.size()>=5){
+            throw new RuntimeException("Maximo 5 productos");
         }
         // para duplicados
         // for (Producto p : carrito) {
         //  if (p.getId() == producto.getId()) return;
         // }
+        producto.setId(contador.getAndIncrement());//id UNICO
         carrito.add(producto);
     }
     public double calcularTotal() {
@@ -38,5 +42,10 @@ public class CarritoService {
         carrito.clear();
     }
 
-    public void eliminarProductoPorId(Long id) { carrito.removeIf(p -> p.getId().equals(id));}
+    public void eliminarProductoPorId(Long id) {
+        carrito.forEach(p ->
+                System.out.println("Producto en carrito ID: " + p.getId())
+        );
+        System.out.println("ID a eliminar: " + id);
+        carrito.removeIf(p -> p.getId().equals(id));}
 }
