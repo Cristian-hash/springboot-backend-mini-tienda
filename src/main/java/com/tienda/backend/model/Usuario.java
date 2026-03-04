3d325510f87ae66d52e7ca2f872bbd01bcb578c6
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-03-04 05:51:08 -0500

    Creacion de la Entidad Usuarios

---
+++ src/main/java/com/tienda/backend/model/Usuario.java
@@ -0,0 +1,12 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Usuario{
+    
+
+
+
+}
