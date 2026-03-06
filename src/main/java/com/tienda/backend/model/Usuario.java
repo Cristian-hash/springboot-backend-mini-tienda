5e85c58f2cb0f90ca884285e55e15ae2ec2ffb3e
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-03-06 05:48:04 -0500

    Creacion de la Entidad Usuario y la connfiguracion

--- src/main/java/com/tienda/backend/model/Usuario.java
+++ src/main/java/com/tienda/backend/model/Usuario.java
@@ -1,19 +1,51 @@
+package com.tienda.backend.model;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
+
+
 @Entity
-public class Usuario{
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nombre;
     private String email;
 
     public Usuario() {
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Usuario(Long id, String nombre, String email){
+        this.id=id;
+        this.nombre=nombre;
+        this.email=email;
+    }
 }
