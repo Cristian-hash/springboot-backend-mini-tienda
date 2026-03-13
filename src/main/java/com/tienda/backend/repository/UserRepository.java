137c86c5529dd42708b5fff816b8e3e57706dfe1
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-03-13 05:52:24 -0500

    creacion del archivo repository y comprension de la difenrencia entre modelo y entidad

---
+++ src/main/java/com/tienda/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tienda.backend.repository;
+
+import com.tienda.backend.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario,Long> {
+
+}
