bc04408549b82e3729317ede0f4b98a2f22d62dc
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-03-15 05:46:18 -0500

    creacion de usuario service y su implmentacion entendiendo

--- src/main/java/com/tienda/backend/repository/UserRepository.java
+++ src/main/java/com/tienda/backend/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.tienda.backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Usuario,Long> {
-
+    //Opcional
+    Optional<Usuario> findByEmail(String email);
 }
+
+
--- src/main/java/com/tienda/backend/service/UsuarioService.java
+++ src/main/java/com/tienda/backend/service/UsuarioService.java
@@ -1,5 +1,11 @@
 package com.tienda.backend.service;
 
+import com.tienda.backend.model.Usuario;
+import com.tienda.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class UsuarioService {
     private final UserRepository repo;
